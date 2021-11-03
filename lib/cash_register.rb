277d6365dcd1b5568ca5b5afa7d43cfe78dbf09a
James Brownie <jamespbrownie@gmail.com>
2021-11-03 09:12:22 -0500

    commit tree 8e039ee54c942b9a3848481c3e7de0e7176a06b9

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
 
+class CashRegister
+    attr_acessor :total
+    @@items = []
+
+    def initialize(employee_discount = 0)
+        @employee_discount = employee_discount
+        @total = 0
+        @previous_total = 0
+    end
+
+    def add_item(title, price, quantity = 1)
+        @previous_total = @total
+        @total = (price * quantity)
+        item = {title: title, price: price, quantity: quantity}
+        @@items << item
+    end
+
+    
+
+end
